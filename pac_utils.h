e797919154a50a7069e7055718824d7205d1bcd2
manugarg <devnull@localhost>
2007-12-02 22:02:27 +0000

    Update COPYING to latest version of GNU LGPL. Add license part to pac_utils.h and Makefile.

--- pac_utils.h
+++ pac_utils.h
@@ -1,3 +1,31 @@
+// Copyright (C) 2007 Manu Garg.
+// Author: Manu Garg <manugarg@gmail.com>
+//
+// pac_utils.h defines some of the functions used by PAC files. This file is
+// packaged with pacparser source code and is required for compiling pacparser.
+// Please read README file included with this package for more information
+// about pacparser.
+
+// Note: This file is derived from "nsProxyAutoConfig.js" file that comes with
+// mozilla source code. Please check out the following for initial developer
+// and contributors:
+//http://lxr.mozilla.org/seamonkey/source/netwerk/base/src/nsProxyAutoConfig.js
+//
+// This file is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// pacparser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+// USA
+
 const char *pacUtils =
 "function dnsDomainIs(host, domain) {\n"
 "    return (host.length >= domain.length &&\n"
