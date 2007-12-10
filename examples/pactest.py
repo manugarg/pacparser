f13c4be5176610ec35514507de576bd06cf54be6
manugarg <devnull@localhost>
2007-12-10 16:24:09 +0000

    Add an example for python too.

---
+++ examples/pactest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.5
+
+from ctypes import *
+
+pp = CDLL("libpacparser.so")
+
+pp.pacparser_init()
+pp.pacparser_parse_pac("examples/wpad.dat")
+proxy = pp.pacparser_find_proxy("http://www.manugarg.com", "www.manugarg.com")
+print string_at(proxy)
+pp.pacparser_cleanup()
+
+# Or simply,
+print string_at(pp.pacparser_just_find_proxy("examples/wpad.dat",
+                                             "http://xyz.manugarg.com",
+                                             "xyz.manugarg.com"))
