f503f954adeb18068fd6d2e7c3dc5a265f3c6aed
manugarg <devnull@localhost>
2010-08-11 23:45:32 +0530

    src/Makefile:  * Create a buildstamp after building jsapi to record that jsapi has been built and don't need to built again.  * Replace /bin/true by true. On Mac, path of true is different.  * Fix path for "examples".
    tools/buildmacpackage:
     * Use "make" to install files.
     * Install python module as well.

--- tools/buildmacpackage.sh
+++ tools/buildmacpackage.sh
@@ -7,19 +7,22 @@ ver=$1
 [ -z $ver ] && echo "Please specify package version." && exit
 major_ver=${ver/.*.*/}
 
-cd $(dirname $0); tools_dir=$PWD; cd -
-
+# $stage_dir is where we'll install our package files.
 stage_dir=/tmp/pacparser_$RANDOM
-
 sudo rm -rf /tmp/pacparser*
-mkdir -p ${stage_dir}/usr/{bin,lib}
+mkdir -p $stage_dir
 
-install -m 555 src/libpacparser.${major_ver}.dylib ${stage_dir}/usr/lib
-ln -sf libpacparser.${major_ver}.dylib ${stage_dir}/usr/lib/libpacparser.dylib
-install -m 555 src/pactester ${stage_dir}/usr/bin
+cd $(dirname $0); tools_dir=$PWD; cd -
+cd $tools_dir/..
+
+# Build pactester and pacparser library and install in $stage_dir
+make -C src
+DESTDIR=$stage_dir make -C src install
+# Build python module and install it in $stage_dir
+make -C src pymod
+DESTDIR=$stage_dir make -C src install-pymod
 
 sudo chown -R root:wheel ${stage_dir}
-
 /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker \
  -v -i com.manugarg -r ${stage_dir} -n ${ver} -t pacparser -m -o pacparser.pkg
 
