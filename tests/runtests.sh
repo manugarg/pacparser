ff918fb206b250b565af77f13879c5b6acacc241
manugarg <devnull@localhost>
2011-09-05 18:59:04 +0530

    * Makefile:          Add a special target 'testpactester_no_internet' for                      building on machines that don't have internet, e.g. debian                      automated build environments. * tests/runtests.sh: Don't run tests that require internet connection if                      NO_INTERNET environment variable is set. * tests/testdata:    Specify INTERNET_REQUIRED for the DNS test.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,49 +2,57 @@
 
 pushd $(dirname $0) > /dev/null; script_dir=$PWD; popd > /dev/null
 
-PACTESTER=$script_dir/../src/pactester
-PACFILE=$script_dir/proxy.pac
-TESTDATA=$script_dir/testdata
-LIBRARY_PATH=$script_dir/../src
-export DYLD_LIBRARY_PATH=$LIBRARY_PATH
-export LD_LIBRARY_PATH=$LIBRARY_PATH
+pactester=$script_dir/../src/pactester
+pacfile=$script_dir/proxy.pac
+testdata=$script_dir/testdata
+library_path=$script_dir/../src
+export DYLD_LIBRARY_PATH=$library_path
+export LD_LIBRARY_PATH=$library_path
 
-LIB=$LIBRARY_PATH/libpacparser.so.1
+lib=$library_path/libpacparser.so.1
 
-OS_ARCH=$(uname -s | sed /\ /s//_/)
-if [ "$OS_ARCH" = "Darwin" ]; then
-  LIB=$LIBRARY_PATH/libpacparser.1.dylib
+os_arch=$(uname -s | sed /\ /s//_/)
+if [ "$os_arch" = "Darwin" ]; then
+  lib=$library_path/libpacparser.1.dylib
 fi
 
-if test ! -f "$LIB"; then
+if test ! -f "$lib"; then
   echo "Test failed. pacparser library not found."
   exit 1
 fi
 
 while read line
   do
-    echo "$line" | grep -q "^#" && continue
-    PARAMS=$(echo "$line"|cut -d"|" -f1)
-    EXPECTED_RESULT=$(echo $line|cut -d"|" -f2)
-    RESULT=$($PACTESTER -p $PACFILE $PARAMS)
+    comment="${line#*#}"
+    line="${line%%#*}"
+    line=${line%"${line##*[^[:space:]]}"}
+    test -z "$line" && continue
+    # If machine is not connected to the internet and a test requires internet
+    # just skip that test.
+    test ! -z $NO_INTERNET && \
+      test "${comment/INTERNET_REQUIRED/}" != "${comment}" && \
+      continue
+    params=${line%%|*}
+    expected_result=${line##*|}
+    result=$($pactester -p $pacfile $params)
     if [ $? != 0 ]; then
       echo "pactester execution failed."
-      echo "Command tried: $PACTESTER -p $PACFILE $PARAMS"
+      echo "Command tried: $pactester -p $pacfile $params"
       echo "Running with debug mode on..."
-      echo "DEBUG=1 $PACTESTER -p $PACFILE $PARAMS"
-      DEBUG=1 $PACTESTER -p $PACFILE $PARAMS
+      echo "DEBUG=1 $pactester -p $pacfile $params"
+      DEBUG=1 $pactester -p $pacfile $params
       exit 1
     fi
     [ $DEBUG ] && echo "Test line: $line"
-    [ $DEBUG ] && echo "Params: $PARAMS"
-    if [ "$RESULT" != "$EXPECTED_RESULT" ]; then
-      echo "Test failed: got \"$RESULT\", expected \"$EXPECTED_RESULT\""
-      echo "Command tried: $PACTESTER -p $PACFILE $PARAMS"
+    [ $DEBUG ] && echo "Params: $params"
+    if [ "$result" != "$expected_result" ]; then
+      echo "Test failed: got \"$result\", expected \"$expected_result\""
+      echo "Command tried: $pactester -p $pacfile $params"
       echo "Running with debug mode on..."
-      echo "DEBUG=1 $PACTESTER -p $PACFILE $PARAMS"
-      DEBUG=1 $PACTESTER -p $PACFILE $PARAMS
+      echo "DEBUG=1 $pactester -p $pacfile $params"
+      DEBUG=1 $pactester -p $pacfile $params
       exit 1;
     fi
-  done < $TESTDATA
+  done < $testdata
 
 echo "All tests were successful."
