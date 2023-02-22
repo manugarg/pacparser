3af11d976d8bd6dfda27bd820d224520476666a1
Manu Garg <manugarg@gmail.com>
2023-02-21 16:32:01 -0800

    Fix setup.py for newer versions

--- src/pymod/setup.py
+++ src/pymod/setup.py
@@ -44,8 +44,19 @@ def setup_dir():
 
 def module_path():
   py_ver = '.'.join([str(x) for x in sys.version_info[0:2]])
-  return glob.glob(os.path.join(setup_dir(), 'build', 'lib*%s' % py_ver))[0]
+  print('Python version: %s' % py_ver)
+  
+  builddir = os.path.join(setup_dir(), 'build')
+  print('Build dir: %s' % builddir)
+  print(os.listdir(builddir))
+  
+  path = glob.glob(os.path.join(builddir, 'lib*%s*' % py_ver))
+  if len(path) != 0:
+    return path[0]
 
+  # Newer python versions sometimes use dot-less notation for python version.
+  path = glob.glob(os.path.join(builddir, 'lib*%s*' % py_ver.replace('.', '')))
+  return path[0]
 
 def sanitize_version(ver):
   ver = ver.strip()
